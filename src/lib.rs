0a316d10566ccb335f9199b8d85096462d7ecfcd
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-03-05 15:57:44 +0100

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+/*********************** GNU General Public License 3.0 ***********************\
+|                                                                              |
+|  Copyright (C) 2023 Kevin Matthes                                            |
+|                                                                              |
+|  This program is free software: you can redistribute it and/or modify        |
+|  it under the terms of the GNU General Public License as published by        |
+|  the Free Software Foundation, either version 3 of the License, or           |
+|  (at your option) any later version.                                         |
+|                                                                              |
+|  This program is distributed in the hope that it will be useful,             |
+|  but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+|  GNU General Public License for more details.                                |
+|                                                                              |
+|  You should have received a copy of the GNU General Public License           |
+|  along with this program.  If not, see <https://www.gnu.org/licenses/>.      |
+|                                                                              |
+\******************************************************************************/
+
+//! # `aeruginous`
+//!
+//! ## Introduction
+//!
+//! `aeruginous` is a time tracking CLI, written in Rust.  Its main purpose is
+//! to create and maintain working hours, stored in fragments named "time
+//! frames".
+//!
+//! ## Meaning of the Name
+//!
+//! When searching a name for this project, one main requirement was to reflect
+//! both the puspose of tracking time as well as the coding language this CLI is
+//! written in, Rust.  The adjective *aeruginous* fulfills both criterions as it
+//! means that the described noun has patina, a special form of rust which
+//! appears after certain period of time has passed.
+
+mod version;
+
+pub use crate::version::{Version, CRATE_VERSION};
+
+/******************************************************************************/
