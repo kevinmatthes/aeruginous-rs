dd3f88e40dc192e9558ee5345884f0b021c26367
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-03-05 18:46:01 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,7 @@
 //! ## Meaning of the Name
 //!
 //! When searching a name for this project, one main requirement was to reflect
-//! both the puspose of tracking time as well as the coding language this CLI is
+//! both the purpose of tracking time as well as the coding language this CLI is
 //! written in, Rust.  The adjective *aeruginous* fulfills both criterions as it
 //! means that the described noun has patina, a special form of rust which
 //! appears after certain period of time has passed.
