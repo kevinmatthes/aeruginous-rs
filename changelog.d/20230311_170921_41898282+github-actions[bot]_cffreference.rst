c7e9d717774e33c2e6307b0e2fc0f34d68c022b7
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-03-13 17:57:13 +0100

    [Enhancement] Implement `cffreference` (#28)
    * GitHub Actions:  Create fresh Scriv fragment
    
    * Create io.rs
    
    * Update io.rs
    
    * Update lib.rs
    
    * Update application.rs
    
    * Update io.rs
    
    * Update lib.rs
    
    * Update application.rs
    
    * Update application.rs
    
    * Update lib.rs
    
    * Update io.rs
    
    * Update lib.rs
    
    * Update application.rs
    
    * Create pattern_reader.rs
    
    * Update lib.rs
    
    * Update io.rs
    
    * Update pattern_reader.rs
    
    * Create pattern_writer.rs
    
    * Update lib.rs
    
    * Update io.rs
    
    * Update pattern_writer.rs
    
    * Update pattern_reader.rs
    
    * Create pattern_io_processor.rs
    
    * Update lib.rs
    
    * Update application.rs
    
    * Delete io.rs
    
    * Update application.rs
    
    * Update pattern_reader.rs
    
    * Update application.rs
    
    * Update application.rs
    
    * Update application.rs
    
    * Update pattern_io_processor.rs
    
    * Update pattern_writer.rs
    
    * Update CHANGELOG.rst
    
    * Update 20230311_170921_41898282+github-actions[bot]_cffreference.rst
    
    * Update 20230311_170921_41898282+github-actions[bot]_cffreference.rst
    
    * Update lib.rs
    
    ---------
    
    Co-authored-by: GitHub Actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ changelog.d/20230311_170921_41898282+github-actions[bot]_cffreference.rst
@@ -0,0 +1,42 @@
+.. A new scriv changelog fragment.
+..
+.. Uncomment the header that is right (remove the leading dots).
+..
+Added
+.....
+
+- cffreference:  creation
+
+- README:  document cffreference
+
+- src/pattern_io_processor.rs:  creation
+
+- src/pattern_reader.rs:  creation
+
+- src/pattern_writer.rs:  creation
+
+Changed
+.......
+
+- src/application.rs:  apply new Rust coding pattern inspired traits
+
+.. Deprecated
+.. ..........
+..
+.. - A bullet item for the Deprecated category.
+..
+.. Fixed
+.. .....
+..
+.. - A bullet item for the Fixed category.
+..
+.. Removed
+.. .......
+..
+.. - A bullet item for the Removed category.
+..
+.. Security
+.. ........
+..
+.. - A bullet item for the Security category.
+..
--- src/application.rs
+++ src/application.rs
@@ -19,12 +19,9 @@
 
 //! The application's subcommands.
 
+use crate::PatternIOProcessor;
 use clap::{Parser, Subcommand};
-use std::{
-  fs::File,
-  io::{stdin, BufRead, BufReader, Write},
-  path::PathBuf,
-};
+use std::path::PathBuf;
 use sysexits::ExitCode;
 
 /// The supported application modes.
@@ -33,6 +30,17 @@ use sysexits::ExitCode;
 /// different behaviour.
 #[derive(Subcommand)]
 pub enum Action {
+  /// Extract the citation information from a given and valid CFF file.
+  Cffreference {
+    /// The CFF file to read from, defaulting to `stdin`, if omitted.
+    #[arg(short = 'i')]
+    input_file: Option<PathBuf>,
+
+    /// The CFF file to write to, defaulting to `stdout`, if omitted.
+    #[arg(short = 'o')]
+    output_file: Option<PathBuf>,
+  },
+
   /// Extract Markdown code from Rust documentation comments.
   Rs2md {
     /// Whether to extract Rust documentation line comments starting with `///`.
@@ -43,17 +51,98 @@ pub enum Action {
     #[arg(long = "outer")]
     extract_outer: Option<bool>,
 
-    /// The Rust files to read from or `stdin`, if omitted.
+    /// The Rust files to read from, defaulting to `stdin`, if omitted.
     #[arg(short = 'i')]
     input_files: Vec<PathBuf>,
 
-    /// The Markdown file to write to or `stdout`, if omitted.
+    /// The Markdown file to write to, defaulting to `stdout`, if omitted.
     #[arg(short = 'o')]
     output_file: Option<PathBuf>,
   },
 }
 
 impl Action {
+  /// Extract the citation information from a given and valid CFF file.
+  fn cffreference(
+    input_file: &Option<PathBuf>,
+    output_file: &Option<PathBuf>,
+  ) -> ExitCode {
+    |s: String| -> String {
+      let mut buffer = String::new();
+      let mut has_preferred_citation = false;
+      let mut has_type = false;
+      let mut references = false;
+
+      for line in s.lines() {
+        if references {
+          match line.chars().next() {
+            Some(' ' | '-') => {}
+            _ => {
+              references = false;
+            }
+          }
+        }
+
+        if !line.is_empty()
+          && !line.starts_with('#')
+          && !line.starts_with("---")
+          && !line.starts_with("...")
+          && !line.starts_with("cff-version:")
+          && !line.starts_with("message:")
+          && !line.starts_with("references:")
+          && !references
+        {
+          if line.starts_with("preferred-citation:") {
+            has_preferred_citation = true;
+          } else if line.starts_with("type:") {
+            has_type = true;
+          }
+
+          buffer.push_str(&(String::from(line) + "\n"));
+        } else if line.starts_with("references:") {
+          references = true;
+        }
+      }
+
+      if has_preferred_citation {
+        let mut preferred_citation_reached = false;
+        let mut result = String::new();
+
+        for line in buffer.lines() {
+          if preferred_citation_reached && line.starts_with(' ') {
+            result.push_str(&(String::from("  ") + line + "\n"));
+          } else if preferred_citation_reached {
+            preferred_citation_reached = false;
+          }
+
+          if line.starts_with("preferred-citation:") {
+            preferred_citation_reached = true;
+          }
+        }
+
+        let mut lines = result.lines();
+
+        lines
+          .next()
+          .map_or_else(String::new, |l| String::from("  - ") + l.trim() + "\n")
+          + &lines.map(|l| String::from(l) + "\n").collect::<String>()
+      } else {
+        let mut lines = buffer.lines();
+
+        (if has_type {
+          lines.next().map_or_else(String::new, |l| {
+            String::from("  - ") + l.trim() + "\n"
+          })
+        } else {
+          String::from("  - type: software\n")
+        }) + &lines
+          .map(|l| String::from("    ") + l + "\n")
+          .collect::<String>()
+      }
+    }
+    .process(input_file, output_file, true, true)
+  }
+
   /// Extract Markdown code from Rust documentation comments.
   fn rs2md(
     extract_inner: Option<bool>,
@@ -61,89 +150,29 @@ impl Action {
     input_files: &Vec<PathBuf>,
     output_file: &Option<PathBuf>,
   ) -> ExitCode {
-    let mut lines = Vec::<u8>::new();
-
-    if input_files.is_empty() {
-      loop {
-        let mut line = String::new();
-
-        match stdin().read_line(&mut line) {
-          Ok(0) => {
-            break;
-          }
-          Ok(_) => lines.append(&mut line.as_bytes().to_vec()),
-          Err(error) => {
-            eprintln!("{error}");
-            return ExitCode::IoErr;
-          }
-        }
-      }
-    } else {
-      for file in input_files {
-        match File::open(file) {
-          Ok(file) => match BufReader::new(file).fill_buf() {
-            Ok(string) => {
-              lines.append(&mut string.to_vec());
-            }
-            Err(error) => {
-              eprintln!("{error}");
-              return ExitCode::IoErr;
-            }
-          },
-          Err(error) => {
-            eprintln!("{error}");
-            return ExitCode::NoInput;
-          }
-        }
-      }
+    |s: String| -> String {
+      s.lines()
+        .map(str::trim_start)
+        .filter(|l| {
+          (extract_inner.unwrap_or(false) && l.starts_with("///"))
+            || (extract_outer.unwrap_or(false) && l.starts_with("//!"))
+        })
+        .map(|l| {
+          String::from(l.chars().skip(4).collect::<String>().trim_end()) + "\n"
+        })
+        .collect::<String>()
     }
-
-    match String::from_utf8(lines) {
-      Ok(lines) => {
-        let lines = lines
-          .lines()
-          .map(str::trim_start)
-          .filter(|l| {
-            (extract_inner.unwrap_or(false) && l.starts_with("///"))
-              || (extract_outer.unwrap_or(false) && l.starts_with("//!"))
-          })
-          .map(|l| {
-            String::from(l.chars().skip(4).collect::<String>().trim_end())
-              + "\n"
-          })
-          .collect::<String>();
-
-        output_file.as_ref().map_or_else(
-          || {
-            print!("{lines}");
-            ExitCode::Ok
-          },
-          |path| match File::create(path) {
-            Ok(mut file) => match file.write(lines.as_bytes()) {
-              Ok(_) => ExitCode::Ok,
-              Err(error) => {
-                eprintln!("{error}");
-                ExitCode::IoErr
-              }
-            },
-            Err(error) => {
-              eprintln!("{error}");
-              ExitCode::CantCreat
-            }
-          },
-        )
-      }
-      Err(error) => {
-        eprintln!("{error}");
-        ExitCode::DataErr
-      }
-    }
+    .process(input_files, output_file, false, true)
   }
 
   /// Execute the selected action.
   #[must_use]
   pub fn run(&self) -> ExitCode {
     match self {
+      Self::Cffreference {
+        input_file,
+        output_file,
+      } => Self::cffreference(input_file, output_file),
       Self::Rs2md {
         extract_inner,
         extract_outer,
