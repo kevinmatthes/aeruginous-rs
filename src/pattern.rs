d410038dadebd8384b1ccfe188a6a19e2bab7841
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-03-17 16:27:48 +0000

    [Documentation] Add Clap v4.1.9 CFF Entry (#42)
    bors r+

--- src/pattern.rs
+++ src/pattern.rs
@@ -30,11 +30,10 @@ pub trait IOProcessor {
   ///
   /// Processing input is a common task.  Often, this input originates from a
   /// stream and needs to be written to another stream after processing it.
-  /// The traits [`PatternReader`][PatternReader] and
-  /// [`PatternWriter`][PatternWriter] offer semantics to handle reading from
-  /// and writing to streams.  This method now adds a convenient connection
-  /// between them as it is designed to be applied on input processing functions
-  /// and closures.
+  /// The traits [`PatternReader`][Reader] and [`PatternWriter`][Writer] offer
+  /// semantics to handle reading from and writing to streams.  This method now
+  /// adds a convenient connection between them as it is designed to be applied
+  /// on input processing functions and closures.
   ///
   /// The result of the operation is indicated by the return value which
   /// originates from the reading and writing processes and can be propagated to
@@ -99,9 +98,9 @@ pub trait Reader {
 
   /// Fill a string buffer with the information from the given stream.
   ///
-  /// This method behaves just like [`read_bytes`][PatternReader::read_bytes]
-  /// but returns a `String` instead of a `Vec<u8>`.  In addition, there are
-  /// further recommendations for error conditions.
+  /// This method behaves just like [`read_bytes`][Reader::read_bytes] but
+  /// returns a `String` instead of a `Vec<u8>`.  In addition, there are further
+  /// recommendations for error conditions.
   ///
   /// # Errors
   ///
@@ -112,11 +111,11 @@ pub trait Reader {
   ///
   /// ## `sysexits::ExitCode::IoErr`
   ///
-  /// See [`read_bytes`][PatternReader::read_bytes].
+  /// See [`read_bytes`][Reader::read_bytes].
   ///
   /// ## `sysexits::ExitCode::NoInput`
   ///
-  /// See [`read_bytes`][PatternReader::read_bytes].
+  /// See [`read_bytes`][Reader::read_bytes].
   fn read_string(&self, show_error_messages: bool) -> Result<String, ExitCode>;
 }
 
@@ -357,7 +356,7 @@ pub trait Writer {
 
   /// Write the given buffer's contents to the given output stream.
   ///
-  /// See [`write_bytes`][PatternWriter::write_bytes] for details.
+  /// See [`write_bytes`][Writer::write_bytes] for details.
   fn write_string(
     &self,
     buffer: &str,
