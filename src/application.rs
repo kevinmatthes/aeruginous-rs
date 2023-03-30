d600cde2df11c0e5c21c23295610dec26ef21b3d
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-03-30 18:28:48 +0200

    Update application.rs

--- src/application.rs
+++ src/application.rs
@@ -17,7 +17,7 @@
 |                                                                              |
 \******************************************************************************/
 
-use crate::{PatternIOProcessor, Result};
+use crate::{PatternIOProcessor};
 use clap::{Parser, Subcommand};
 use std::path::PathBuf;
 use sysexits::ExitCode;
@@ -64,7 +64,7 @@ impl Action {
   fn cffreference(
     input_file: &Option<PathBuf>,
     output_file: &Option<PathBuf>,
-  ) -> Result<()> {
+  ) -> ExitCode {
     |s: String| -> String {
       let mut buffer = String::new();
       let mut has_preferred_citation = false;
@@ -138,7 +138,7 @@ impl Action {
           .collect::<String>()
       }
     }
-    .io_append(input_file, output_file)
+    .io_append(input_file, output_file).map_or_else(|code| code, |()| ExitCode::Ok)
   }
 
   /// Extract Markdown code from Rust documentation comments.
@@ -147,7 +147,7 @@ impl Action {
     extract_outer: bool,
     input_files: &Vec<PathBuf>,
     output_file: &Option<PathBuf>,
-  ) -> Result<()> {
+  ) -> ExitCode {
     |s: String| -> String {
       s.lines()
         .map(str::trim_start)
@@ -160,13 +160,13 @@ impl Action {
         })
         .collect::<String>()
     }
-    .io(input_files, output_file)
+    .io(input_files, output_file).map_or_else(|code| code, |()| ExitCode::Ok)
   }
 
   /// Execute the selected action.
   #[must_use]
   pub fn run(&self) -> ExitCode {
-    match match self {
+    match self {
       Self::Cffreference {
         input_file,
         output_file,
@@ -179,9 +179,6 @@ impl Action {
       } => {
         Self::rs2md(*extract_inner, *extract_outer, input_files, output_file)
       }
-    } {
-      Ok(()) => ExitCode::Ok,
-      Err(code) => code,
     }
   }
 }
