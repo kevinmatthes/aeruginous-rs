f8feb10a732f7ea50feced76e4e7417e5c715dbe
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-05-06 15:38:51 +0200

    Create running.rs

---
+++ tests/running.rs
@@ -0,0 +1,33 @@
+/*********************** GNU General Public License 3.0 ***********************\
+|                                                                              |
+|  Copyright (C) 2023 Kevin Matthes                                            |
+|                                                                              |
+|  This program is free software: you can redistribute it and/or modify        |
+|  it under the terms of the GNU General Public License as published by        |
+|  the Free Software Foundation, either version 3 of the License, or           |
+|  (at your option) any later version.                                         |
+|                                                                              |
+|  This program is distributed in the hope that it will be useful,             |
+|  but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+|  GNU General Public License for more details.                                |
+|                                                                              |
+|  You should have received a copy of the GNU General Public License           |
+|  along with this program.  If not, see <https://www.gnu.org/licenses/>.      |
+|                                                                              |
+\******************************************************************************/
+
+use aeruginous::Running;
+use chrono::Local;
+
+#[test]
+fn beginning_field_compared_to_now() {
+  assert!(Running::default().beginning() <= &Local::now());
+}
+
+#[test]
+fn method_equality_constructor_and_default() {
+  assert!(Running::default().beginning() <= Running::new().beginning());
+}
+
+/******************************************************************************/
