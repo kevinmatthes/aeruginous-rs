8b76cc470f368e1b8a50b4c86903acb538c734d4
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-05-30 23:52:33 +0200

    Changed ::= comment-changes:  break library API

--- src/comment_changes.rs
+++ src/comment_changes.rs
@@ -22,72 +22,142 @@ use git2::Repository;
 use std::collections::HashMap;
 use sysexits::{ExitCode, Result};
 
-/// Create comments on the latest changes to this repository.
-pub struct CommentChangesData {
-  /// Whether to query the commit messages' bodies rather than their summaries.
+/// Create comments on the commits of a branch in this repository.
+#[derive(clap::Parser, Clone)]
+#[command(aliases = ["changelog"])]
+pub struct CommentChanges {
+  /// Work with the commit messages' bodies instead of their summaries.
+  #[arg(long, short = 'b')]
   body: bool,
 
-  /// The allowed categories.
-  categories: Vec<String>,
-
-  /// The changes to report.
-  changes: HashMap<String, Vec<String>>,
+  /// Only these categories shall be used to generate comments.
+  #[arg(long, short = 'c')]
+  category: Vec<String>,
 
   /// The delimiter to separate a category from the change description.
+  #[arg(long, short = 'd')]
   delimiter: String,
 
-  /// The count of commits to analyse.
+  /// The count of commits to analyse, defaulting to infinity, if omitted.
+  #[arg(aliases = ["count"], long, short = 'n')]
   depth: Option<usize>,
 
-  /// The hyperlinks to define in the output file.
-  hyperlinks: HashMap<String, String>,
+  /// The target format of the resulting fragment.
+  #[arg(
+      aliases = ["format"],
+      default_value = "rst",
+      long,
+      short = 'f',
+      value_parser = |f: &str| {
+        if ["md", "ron", "rst"].contains(&f) {
+          Ok(f.to_string())
+        } else {
+          Err(format!("extension '{f}' is not supported, yet"))
+        }
+      }
+    )]
+  extension: String,
 
-  /// This repository.
-  repository: Option<Repository>,
+  /// The default category to assign.
+  #[arg(long, short = 'C')]
+  fallback_category: Option<String>,
+
+  /// The heading's level in the resulting fragment.
+  #[arg(
+      aliases = ["level"],
+      default_value = "3",
+      long,
+      short = 'H',
+      value_parser = clap::value_parser!(u8).range(1..=3)
+    )]
+  heading: u8,
+
+  /// Set categories Added, Changed, Deprecated, Fixed, Removed, and Security.
+  #[arg(long, short = 'k')]
+  keep_a_changelog: bool,
+
+  /// The hyperlinks to add as comments.
+  #[arg(aliases = ["hyperlink"], long, short = 'l')]
+  link: Vec<String>,
+
+  /// The directory to write the generated fragment to.
+  #[arg(
+      aliases = ["dir", "directory"],
+      default_value = ".",
+      long = "output",
+      short = 'o'
+    )]
+  output_directory: String,
+
+  /// The hyperlinks' targets.
+  #[arg(long, short = 't')]
+  target: Vec<String>,
 }
 
-impl CommentChangesData {
-  /// What is the name of the current branch?
+impl CommentChanges {
+  /// Process the input data.
   ///
   /// # Errors
   ///
+  /// - [`sysexits::ExitCode::DataErr`]
+  /// - [`sysexits::ExitCode::Software`]
   /// - [`sysexits::ExitCode::Unavailable`]
-  /// - See [`Self::open_repository`].
-  pub fn branch_name(&mut self) -> Result<String> {
-    if self.repository.is_none() {
-      self.open_repository()?;
-    }
-
-    let Some(repository) = &self.repository else { unreachable!() };
-
-    repository.head().map_or_else(
-      |error| {
-        eprintln!("{error}");
-        Err(ExitCode::Unavailable)
-      },
-      |reference| {
-        reference
-          .name()
-          .map_or(Err(ExitCode::Unavailable), |name| Ok(name.to_string()))
-      },
-    )
+  /// - [`sysexits::ExitCode::Usage`]
+  pub fn main(&self) -> Result<()> {
+    self.wrap().main()
   }
 
-  /// Generate the changelog fragment.
+  /// Create a new instance.
   #[must_use]
-  pub fn generate_changelog_fragment(
-    &self,
-    heading: u8,
-    extension: &str,
-  ) -> String {
-    let mut result = self.resolve_links(extension);
+  pub fn new(delimiter: String) -> Self {
+    Self {
+      body: false,
+      category: Vec::new(),
+      delimiter,
+      depth: None,
+      extension: "rst".to_string(),
+      fallback_category: None,
+      heading: 3,
+      keep_a_changelog: false,
+      link: Vec::new(),
+      output_directory: ".".to_string(),
+      target: Vec::new(),
+    }
+  }
+
+  fn wrap(&self) -> Logic {
+    Logic {
+      branch: String::new(),
+      categories: Vec::new(),
+      changes: HashMap::new(),
+      cli: self.clone(),
+      hyperlinks: HashMap::new(),
+      repository: None,
+      user: String::new(),
+    }
+  }
+}
+
+struct Logic {
+  branch: String,
+  categories: Vec<String>,
+  changes: HashMap<String, Vec<String>>,
+  cli: CommentChanges,
+  hyperlinks: HashMap<String, String>,
+  repository: Option<Repository>,
+  user: String,
+}
+
+impl Logic {
+  fn generate(&self) -> String {
+    let mut result = self.resolve();
 
     for (category, changes) in &self.changes {
-      result.append_as_line(match extension {
-        "md" => format!("{} {category}\n", "#".repeat(heading.into())),
+      result.append_as_line(match self.cli.extension.as_str() {
+        "md" => format!("{} {category}\n", "#".repeat(self.cli.heading.into())),
         "rst" => format!(
           "{category}\n{}\n",
-          match heading {
+          match self.cli.heading {
             1 => "=",
             2 => "-",
             3 => ".",
@@ -106,8 +176,54 @@ impl CommentChangesData {
     result
   }
 
-  /// Insert a new change into this instance's set of changes.
-  fn insert_change(
+  fn get_branch(&mut self) -> Result<()> {
+    if let Some(repository) = &self.repository {
+      self.branch = repository.head().map_or_else(
+        |error| {
+          eprintln!("{error}");
+          Err(ExitCode::Unavailable)
+        },
+        |reference| {
+          reference
+            .name()
+            .map_or(Err(ExitCode::Unavailable), |name| Ok(name.to_string()))
+        },
+      )?;
+
+      Ok(())
+    } else {
+      Err(ExitCode::Software)
+    }
+  }
+
+  fn get_user(&mut self) -> Result<()> {
+    if let Some(repository) = &self.repository {
+      self.user = repository
+        .config()
+        .map_or_else(
+          |error| {
+            eprintln!("{error}");
+            Err(ExitCode::Unavailable)
+          },
+          |config| {
+            config.get_string("user.name").map_or_else(
+              |_| {
+                eprintln!("There is no Git username configured, yet.");
+                Err(ExitCode::DataErr)
+              },
+              Ok,
+            )
+          },
+        )?
+        .replace(' ', "_");
+
+      Ok(())
+    } else {
+      Err(ExitCode::Software)
+    }
+  }
+
+  fn insert(
     map: &mut HashMap<String, Vec<String>>,
     category: String,
     change: String,
@@ -121,195 +237,162 @@ impl CommentChangesData {
     map.insert(category, changes);
   }
 
-  /// Analyse the latest changes and create a report.
-  ///
-  /// # Errors
-  ///
-  /// See
-  ///
-  /// - [`Self::query_last_n_commits`]
-  /// - [`Self::report_changes`]
-  pub fn main(
-    &mut self,
-    output_directory: &str,
-    heading: u8,
-    extension: &str,
-    fallback_category: &Option<String>,
-  ) -> Result<()> {
-    self.query_last_n_commits(fallback_category)?;
-    self.report_changes(output_directory, heading, extension)
+  fn main(&mut self) -> Result<()> {
+    self.preprocess()?;
+    self.query()?;
+    self.report()
   }
 
-  /// Create a new instance from the command line arguments.
-  #[must_use]
-  pub fn new(
-    depth: Option<usize>,
-    delimiter: String,
-    hyperlinks: HashMap<String, String>,
-    categories: Vec<String>,
-    body: bool,
-  ) -> Self {
-    Self {
-      body,
-      categories,
-      changes: HashMap::new(),
-      delimiter,
-      depth,
-      hyperlinks,
-      repository: None,
+  fn preprocess(&mut self) -> Result<()> {
+    if self.cli.keep_a_changelog {
+      self.categories.append(
+        &mut vec![
+          "Added",
+          "Changed",
+          "Deprecated",
+          "Fixed",
+          "Removed",
+          "Security",
+        ]
+        .iter()
+        .map(ToString::to_string)
+        .collect::<Vec<String>>(),
+      );
     }
-  }
 
-  /// Open this repository.
-  ///
-  /// # Errors
-  ///
-  /// - [`sysexits::ExitCode::Usage`]
-  pub fn open_repository(&mut self) -> Result<()> {
+    self.categories.append(&mut self.cli.category.clone());
+
+    self.hyperlinks = self
+      .cli
+      .link
+      .iter()
+      .zip(self.cli.target.iter())
+      .map(|(a, b)| (a.to_string(), b.to_string()))
+      .collect();
+
     Repository::open(".").map_or_else(
       |_| {
         eprintln!("This is not a Git repository.");
         Err(ExitCode::Usage)
       },
-      |repository| {
-        self.repository = Some(repository);
+      |r| {
+        self.repository = Some(r);
         Ok(())
       },
     )
   }
 
-  /// Query the given amount of commits.
-  ///
-  /// # Errors
-  ///
-  /// - [`sysexits::ExitCode::Unavailable`]
-  /// - See [`Self::open_repository`].
-  pub fn query_last_n_commits(
-    &mut self,
-    fallback_category: &Option<String>,
-  ) -> Result<()> {
-    if self.repository.is_none() {
-      self.open_repository()?;
-    }
+  fn query(&mut self) -> Result<()> {
+    if let Some(repository) = &self.repository {
+      match repository.revwalk() {
+        Ok(mut revwalk) => match revwalk.push_head() {
+          Ok(()) => {
+            let mut count = 1;
+            let mut result = HashMap::new();
 
-    let Some(repository) = &self.repository else { unreachable!() };
-
-    match repository.revwalk() {
-      Ok(mut revwalk) => match revwalk.push_head() {
-        Ok(()) => {
-          let mut count = 1;
-          let mut result = HashMap::new();
-
-          for oid in revwalk {
-            if let Some(depth) = self.depth {
-              if count > depth {
-                break;
+            for oid in revwalk {
+              if let Some(depth) = self.cli.depth {
+                if count > depth {
+                  break;
+                }
               }
-            }
 
-            if let Ok(oid) = oid {
-              if let Ok(commit) = repository.find_commit(oid) {
-                if let Some(message) = if self.body {
-                  commit.body()
-                } else {
-                  commit.summary()
-                } {
-                  if let Some((category, change)) =
-                    message.trim().split_once(&self.delimiter)
-                  {
-                    let category = category.trim().to_string();
-                    let change = change.trim().to_string();
-                    let valid_category =
-                      self.categories.iter().any(|c| c == &category);
+              if let Ok(oid) = oid {
+                if let Ok(commit) = repository.find_commit(oid) {
+                  if let Some(message) = if self.cli.body {
+                    commit.body()
+                  } else {
+                    commit.summary()
+                  } {
+                    if let Some((category, change)) =
+                      message.trim().split_once(&self.cli.delimiter)
+                    {
+                      let category = category.trim().to_string();
+                      let change = change.trim().to_string();
+                      let valid_category =
+                        self.categories.iter().any(|c| c == &category);
 
-                    if self.categories.is_empty() || valid_category {
-                      Self::insert_change(&mut result, category, change);
-                    } else if !valid_category && fallback_category.is_some() {
-                      let Some(fallback) = fallback_category else { unreachable!() };
-                      Self::insert_change(
+                      if self.categories.is_empty() || valid_category {
+                        Self::insert(&mut result, category, change);
+                      } else if !valid_category {
+                        if let Some(fallback) = &self.cli.fallback_category {
+                          Self::insert(
+                            &mut result,
+                            fallback.to_string(),
+                            change,
+                          );
+                        }
+                      }
+                    } else if let Some(fallback) = &self.cli.fallback_category {
+                      Self::insert(
                         &mut result,
                         fallback.to_string(),
-                        change,
+                        message.trim().to_string(),
                       );
                     }
-                  } else if let Some(fallback) = fallback_category {
-                    Self::insert_change(
-                      &mut result,
-                      fallback.to_string(),
-                      message.trim().to_string(),
-                    );
                   }
+                } else {
+                  eprintln!("Commit {oid} does not seem to exist.");
+                  return Err(ExitCode::DataErr);
                 }
               } else {
-                eprintln!("Commit {oid} does not seem to exist.");
-                return Err(ExitCode::DataErr);
+                eprintln!("There were not enough commits fetched on checkout.");
+                return Err(ExitCode::Usage);
               }
-            } else {
-              eprintln!("There were not enough commits fetched on checkout.");
-              return Err(ExitCode::Usage);
+
+              count += 1;
             }
 
-            count += 1;
+            self.changes = result;
+
+            Ok(())
           }
-
-          self.changes = result;
-
-          Ok(())
-        }
+          Err(error) => {
+            eprintln!("{error}");
+            Err(ExitCode::Unavailable)
+          }
+        },
         Err(error) => {
           eprintln!("{error}");
           Err(ExitCode::Unavailable)
         }
-      },
-      Err(error) => {
-        eprintln!("{error}");
-        Err(ExitCode::Unavailable)
       }
+    } else {
+      Err(ExitCode::Software)
     }
   }
 
-  /// Report the logged changes.
-  ///
-  /// # Errors
-  ///
-  /// See
-  ///
-  /// - [`Self::branch_name`]
-  /// - [`Self::who_am_i`]
-  pub fn report_changes(
-    &mut self,
-    output_directory: &str,
-    heading: u8,
-    extension: &str,
-  ) -> Result<()> {
-    let branch = self.branch_name()?;
-    let content = if extension == "ron" {
+  pub fn report(&mut self) -> Result<()> {
+    let content = if self.cli.extension == "ron" {
       ron::ser::to_string_pretty(
         &Fragment::new(&self.hyperlinks, &self.changes),
         ron::ser::PrettyConfig::default().indentor("  ".to_string()),
       )
       .map_or(Err(ExitCode::DataErr), Ok)?
     } else {
-      self.generate_changelog_fragment(heading, extension)
+      self.generate()
     };
-    let user = self.who_am_i()?.replace(' ', "_");
+
+    self.get_branch()?;
+    self.get_user()?;
 
     format!(
-      "{output_directory}/{}_{user}_{}.{extension}",
+      "{}/{}_{}_{}.{}",
+      self.cli.output_directory,
       chrono::Local::now().format("%Y%m%d_%H%M%S"),
-      branch.split('/').last().unwrap_or("HEAD")
+      self.user,
+      self.branch.split('/').last().unwrap_or("HEAD"),
+      self.cli.extension
     )
     .write(Box::new(content))
   }
 
-  /// Assemble the links for the resulting report.
-  #[must_use]
-  pub fn resolve_links(&self, extension: &str) -> String {
+  fn resolve(&self) -> String {
     let mut result = String::new();
 
     if !self.hyperlinks.is_empty() {
       for (link_name, target) in &self.hyperlinks {
-        result.append_as_line(match extension {
+        result.append_as_line(match self.cli.extension.as_str() {
           "md" => format!("[{link_name}]:  {target}"),
           "rst" => format!(".. _{link_name}:  {target}"),
           _ => unreachable!(),
@@ -320,37 +403,6 @@ impl CommentChangesData {
     }
 
     result
-  }
-
-  /// Who is configured as user?
-  ///
-  /// # Errors
-  ///
-  /// - [`sysexits::ExitCode::DataErr`]
-  /// - [`sysexits::ExitCode::Unavailable`]
-  /// - See [`Self::open_repository`].
-  pub fn who_am_i(&mut self) -> Result<String> {
-    if self.repository.is_none() {
-      self.open_repository()?;
-    }
-
-    let Some(repository) = &self.repository else { unreachable!() };
-
-    repository.config().map_or_else(
-      |error| {
-        eprintln!("{error}");
-        Err(ExitCode::Unavailable)
-      },
-      |config| {
-        config.get_string("user.name").map_or_else(
-          |_| {
-            eprintln!("There is no Git username configured, yet.");
-            Err(ExitCode::DataErr)
-          },
-          Ok,
-        )
-      },
-    )
   }
 }
 
