ea8d10064ff76ff62e9c66c72a3409621c820c3f
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-05-30 02:12:05 +0200

    Added ::= tests/cffreference.rs:  creation

---
+++ tests/cffreference.rs
@@ -0,0 +1,58 @@
+/*********************** GNU General Public License 3.0 ***********************\
+|                                                                              |
+|  Copyright (C) 2023 Kevin Matthes                                            |
+|                                                                              |
+|  This program is free software: you can redistribute it and/or modify        |
+|  it under the terms of the GNU General Public License as published by        |
+|  the Free Software Foundation, either version 3 of the License, or           |
+|  (at your option) any later version.                                         |
+|                                                                              |
+|  This program is distributed in the hope that it will be useful,             |
+|  but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+|  GNU General Public License for more details.                                |
+|                                                                              |
+|  You should have received a copy of the GNU General Public License           |
+|  along with this program.  If not, see <https://www.gnu.org/licenses/>.      |
+|                                                                              |
+\******************************************************************************/
+
+use aeruginous::{Cffreference, PatternReader};
+use std::{fs::remove_file, path::PathBuf};
+
+macro_rules! make_test {
+  ( $n:tt -> $name:ident ) => {
+    #[test]
+    fn $name() {
+      Cffreference::new(
+        Some(PathBuf::from(concat!("./cffs/input_", $n, ".cff"))),
+        Some(PathBuf::from(concat!("./cffs/output_", $n, ".cff"))),
+      )
+      .main()
+      .unwrap();
+
+      assert_eq!(
+        "./cffs/expectation.cff"
+          .read()
+          .unwrap()
+          .try_into_string()
+          .unwrap(),
+        concat!("./cffs/output_", $n, ".cff")
+          .read()
+          .unwrap()
+          .try_into_string()
+          .unwrap()
+      );
+
+      remove_file(concat!("./cffs/output_", $n, ".cff")).unwrap();
+    }
+  };
+}
+
+make_test!(1 -> classic);
+make_test!(2 -> data_after_references);
+make_test!(3 -> has_type);
+make_test!(4 -> has_preferred_citation);
+make_test!(5 -> data_after_preferred_citation);
+
+/******************************************************************************/
