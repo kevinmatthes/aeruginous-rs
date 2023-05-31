19db7d5a39d43f28afd9a165f79d0433a010c667
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-05-31 04:50:25 +0200

    Added ::= ToRon

--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -21,12 +21,14 @@ mod append_as_line;
 mod colour_message;
 mod convert_buffer;
 mod prefer;
+mod to_ron;
 mod to_stderr;
 
 pub use append_as_line::AppendAsLine;
 pub use colour_message::ColourMessage;
 pub use convert_buffer::ConvertBuffer;
 pub use prefer::Prefer;
+pub use to_ron::ToRon;
 pub use to_stderr::ToStderr;
 
 /******************************************************************************/
