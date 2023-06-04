2f2e965d26e8db1dec7dbf1b361e5d1f4ba05ee8
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-06-04 02:01:32 +0200

    Added ::= tests/ron_traits.rs

---
+++ tests/ron_traits.rs
@@ -0,0 +1,35 @@
+/*********************** GNU General Public License 3.0 ***********************\
+|                                                                              |
+|  Copyright (C) 2023 Kevin Matthes                                            |
+|                                                                              |
+|  This program is free software: you can redistribute it and/or modify        |
+|  it under the terms of the GNU General Public License as published by        |
+|  the Free Software Foundation, either version 3 of the License, or           |
+|  (at your option) any later version.                                         |
+|                                                                              |
+|  This program is distributed in the hope that it will be useful,             |
+|  but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+|  GNU General Public License for more details.                                |
+|                                                                              |
+|  You should have received a copy of the GNU General Public License           |
+|  along with this program.  If not, see <https://www.gnu.org/licenses/>.      |
+|                                                                              |
+\******************************************************************************/
+
+use aeruginous::{FromRon, ToRon};
+
+#[test]
+fn i32_number_struct() {
+  #[derive(Debug, Eq, serde::Deserialize, serde::Serialize, PartialEq)]
+  struct Number {
+    n: i32,
+  }
+
+  assert_eq!(
+    Number::from_ron(&Number { n: 42 }.to_ron(2).unwrap()).unwrap(),
+    Number { n: 42 }
+  );
+}
+
+/******************************************************************************/
