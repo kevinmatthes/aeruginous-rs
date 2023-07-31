b34f1f3492eb22c4ef634c446af7d8766b2f65cc
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-07-31 19:27:02 +0200

    Update macros.rs

--- tests/macros.rs
+++ tests/macros.rs
@@ -22,255 +22,255 @@ use std::str::FromStr;
 
 #[test]
 fn enum_trait() {
-  #[derive(Debug, PartialEq)]
-  enum E {
-    A,
-    B,
-  }
+    #[derive(Debug, PartialEq)]
+    enum E {
+        A,
+        B,
+    }
 
-  enum_trait!(E {
-    A <-> "a",
-    B <-> "b"
-  });
+    enum_trait!(E {
+      A <-> "a",
+      B <-> "b"
+    });
 
-  assert_eq!(&E::A.to_string(), "a");
-  assert_eq!(&E::B.to_string(), "b");
-  assert_eq!(E::from_str("a").unwrap(), E::A);
-  assert_eq!(E::from_str("b").unwrap(), E::B);
-  assert!(E::from_str("?").is_err());
+    assert_eq!(&E::A.to_string(), "a");
+    assert_eq!(&E::B.to_string(), "b");
+    assert_eq!(E::from_str("a").unwrap(), E::A);
+    assert_eq!(E::from_str("b").unwrap(), E::B);
+    assert!(E::from_str("?").is_err());
 }
 
 #[test]
 fn getters_cp_one_field() {
-  struct Example {
-    a: i32,
-  }
+    struct Example {
+        a: i32,
+    }
 
-  getters!(@cp Example {
-    a: i32
-  });
+    getters!(@cp Example {
+      a: i32
+    });
 
-  let example = Example { a: 42 };
+    let example = Example { a: 42 };
 
-  assert_eq!(example.a(), 42);
+    assert_eq!(example.a(), 42);
 }
 
 #[test]
 fn getters_cp_two_fields() {
-  struct Example {
-    a: i32,
-    b: f64,
-  }
+    struct Example {
+        a: i32,
+        b: f64,
+    }
 
-  getters!(@cp Example {
-    a: i32,
-    b: f64
-  });
+    getters!(@cp Example {
+      a: i32,
+      b: f64
+    });
 
-  let example = Example { a: 42, b: 23.0 };
+    let example = Example { a: 42, b: 23.0 };
 
-  assert_eq!(example.a(), 42);
-  assert_eq!(example.b(), 23.0);
+    assert_eq!(example.a(), 42);
+    assert_eq!(example.b(), 23.0);
 }
 
 #[test]
 fn getters_fn_cp_one_field() {
-  struct Example {
-    a: i32,
-  }
-
-  impl Example {
-    getters!(@fn @cp
-      a: i32
-    );
-
-    pub fn function() -> i32 {
-      42
+    struct Example {
+        a: i32,
     }
-  }
 
-  let example = Example { a: 42 };
+    impl Example {
+        getters!(@fn @cp
+          a: i32
+        );
 
-  assert_eq!(example.a(), 42);
-  assert_eq!(Example::function(), 42);
+        pub fn function() -> i32 {
+            42
+        }
+    }
+
+    let example = Example { a: 42 };
+
+    assert_eq!(example.a(), 42);
+    assert_eq!(Example::function(), 42);
 }
 
 #[test]
 fn getters_fn_cp_two_fields() {
-  struct Example {
-    a: i32,
-    b: f64,
-  }
-
-  impl Example {
-    getters!(@fn @cp
-      a: i32,
-      b: f64
-    );
-
-    pub fn function() -> i32 {
-      42
+    struct Example {
+        a: i32,
+        b: f64,
     }
-  }
 
-  let example = Example { a: 42, b: 23.0 };
+    impl Example {
+        getters!(@fn @cp
+          a: i32,
+          b: f64
+        );
 
-  assert_eq!(example.a(), 42);
-  assert_eq!(example.b(), 23.0);
-  assert_eq!(Example::function(), 42);
+        pub fn function() -> i32 {
+            42
+        }
+    }
+
+    let example = Example { a: 42, b: 23.0 };
+
+    assert_eq!(example.a(), 42);
+    assert_eq!(example.b(), 23.0);
+    assert_eq!(Example::function(), 42);
 }
 
 #[test]
 fn getters_fn_ref_one_field() {
-  struct Example {
-    a: String,
-  }
-
-  impl Example {
-    getters!(@fn @ref
-      a: String
-    );
-
-    pub fn function() -> i32 {
-      42
+    struct Example {
+        a: String,
     }
-  }
 
-  let example = Example {
-    a: "string".to_string(),
-  };
+    impl Example {
+        getters!(@fn @ref
+          a: String
+        );
 
-  assert_eq!(example.a(), "string");
-  assert_eq!(Example::function(), 42);
+        pub fn function() -> i32 {
+            42
+        }
+    }
+
+    let example = Example {
+        a: "string".to_string(),
+    };
+
+    assert_eq!(example.a(), "string");
+    assert_eq!(Example::function(), 42);
 }
 
 #[test]
 fn getters_fn_ref_two_fields() {
-  struct Example {
-    a: String,
-    b: Vec<i32>,
-  }
-
-  impl Example {
-    getters!(@fn @ref
-      a: String,
-      b: Vec<i32>
-    );
-
-    pub fn function() -> i32 {
-      42
+    struct Example {
+        a: String,
+        b: Vec<i32>,
     }
-  }
 
-  let example = Example {
-    a: "string".to_string(),
-    b: vec![1, 2, 3],
-  };
+    impl Example {
+        getters!(@fn @ref
+          a: String,
+          b: Vec<i32>
+        );
 
-  assert_eq!(example.a(), "string");
-  assert_eq!(example.b(), &[1, 2, 3]);
-  assert_eq!(Example::function(), 42);
+        pub fn function() -> i32 {
+            42
+        }
+    }
+
+    let example = Example {
+        a: "string".to_string(),
+        b: vec![1, 2, 3],
+    };
+
+    assert_eq!(example.a(), "string");
+    assert_eq!(example.b(), &[1, 2, 3]);
+    assert_eq!(Example::function(), 42);
 }
 
 #[test]
 fn getters_header() {
-  struct Example {
-    a: i32,
-    b: f64,
-    c: bool,
-  }
+    struct Example {
+        a: i32,
+        b: f64,
+        c: bool,
+    }
 
-  impl Example {
-    getters!(@fn @cp
-      a: i32,
-      b: f64
-    );
+    impl Example {
+        getters!(@fn @cp
+          a: i32,
+          b: f64
+        );
 
-    getters!(@header c = (
-      pub fn field_c(&self) -> String {
-        self.c.to_string()
-      }
-    ));
-  }
+        getters!(@header c = (
+          pub fn field_c(&self) -> String {
+            self.c.to_string()
+          }
+        ));
+    }
 
-  let example = Example {
-    a: 42,
-    b: 23.0,
-    c: true,
-  };
+    let example = Example {
+        a: 42,
+        b: 23.0,
+        c: true,
+    };
 
-  assert_eq!(example.a(), 42);
-  assert_eq!(example.b(), 23.0);
-  assert_eq!(example.field_c(), "true".to_string());
+    assert_eq!(example.a(), 42);
+    assert_eq!(example.b(), 23.0);
+    assert_eq!(example.field_c(), "true".to_string());
 }
 
 #[test]
 fn getters_ref_one_field() {
-  struct Example {
-    a: String,
-  }
+    struct Example {
+        a: String,
+    }
 
-  getters!(@ref Example {
-    a: String
-  });
+    getters!(@ref Example {
+      a: String
+    });
 
-  let example = Example {
-    a: "string".to_string(),
-  };
+    let example = Example {
+        a: "string".to_string(),
+    };
 
-  assert_eq!(example.a(), "string");
+    assert_eq!(example.a(), "string");
 }
 
 #[test]
 fn getters_ref_two_fields() {
-  struct Example {
-    a: String,
-    b: Vec<i32>,
-  }
+    struct Example {
+        a: String,
+        b: Vec<i32>,
+    }
 
-  getters!(@ref Example {
-    a: String,
-    b: Vec<i32>
-  });
+    getters!(@ref Example {
+      a: String,
+      b: Vec<i32>
+    });
 
-  let example = Example {
-    a: "string".to_string(),
-    b: vec![1, 2, 3],
-  };
+    let example = Example {
+        a: "string".to_string(),
+        b: vec![1, 2, 3],
+    };
 
-  assert_eq!(example.a(), "string");
-  assert_eq!(example.b(), &[1, 2, 3]);
+    assert_eq!(example.a(), "string");
+    assert_eq!(example.b(), &[1, 2, 3]);
 }
 
 #[test]
 fn implement_one_function() {
-  struct Example;
+    struct Example;
 
-  implement!(Example;
-    pub fn function() -> i32 {
-      42
-    }
-  );
+    implement!(Example;
+      pub fn function() -> i32 {
+        42
+      }
+    );
 
-  assert_eq!(Example::function(), 42);
+    assert_eq!(Example::function(), 42);
 }
 
 #[test]
 fn implement_two_functions() {
-  struct Example;
+    struct Example;
 
-  implement!(Example;
-    pub fn function_one() -> i32 {
-      42
-    },
+    implement!(Example;
+      pub fn function_one() -> i32 {
+        42
+      },
 
-    pub fn function_two() -> f64 {
-      23.0
-    }
-  );
+      pub fn function_two() -> f64 {
+        23.0
+      }
+    );
 
-  assert_eq!(Example::function_one(), 42);
-  assert_eq!(Example::function_two(), 23.0);
+    assert_eq!(Example::function_one(), 42);
+    assert_eq!(Example::function_two(), 23.0);
 }
 
 /******************************************************************************/
