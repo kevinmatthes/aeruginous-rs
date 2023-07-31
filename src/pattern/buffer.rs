32fb748340d5c06f8ead02dca60c8d507363db72
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-07-31 19:16:38 +0200

    Update buffer.rs

--- src/pattern/buffer.rs
+++ src/pattern/buffer.rs
@@ -21,90 +21,93 @@ use sysexits::{ExitCode, Result};
 
 /// A buffer for pattern-based IO.
 pub trait Buffer {
-  /// Fill this buffer with bytes.
-  ///
-  /// Some reading operations return the read data as a sequence of UTF-8
-  /// encoded characters.  This method will convert the given data into the
-  /// type of the buffer.  If the conversion of the given data succeeds, the
-  /// buffer's contents will be replaced with the converted data.
-  ///
-  /// # Errors
-  ///
-  /// - [`sysexits::ExitCode::DataErr`]
-  fn try_from_bytes(&mut self, bytes: &[u8]) -> Result<()>;
+    /// Fill this buffer with bytes.
+    ///
+    /// Some reading operations return the read data as a sequence of UTF-8
+    /// encoded characters.  This method will convert the given data into the
+    /// type of the buffer.  If the conversion of the given data succeeds, the
+    /// buffer's contents will be replaced with the converted data.
+    ///
+    /// # Errors
+    ///
+    /// - [`sysexits::ExitCode::DataErr`]
+    fn try_from_bytes(&mut self, bytes: &[u8]) -> Result<()>;
 
-  /// Fill this buffer with a string.
-  ///
-  /// This method behaves just like [`Self::try_from_bytes`] but for [`String`]
-  /// as the respective source type.
-  ///
-  /// # Errors
-  ///
-  /// - [`sysexits::ExitCode::DataErr`]
-  fn try_from_string(&mut self, string: &str) -> Result<()>;
+    /// Fill this buffer with a string.
+    ///
+    /// This method behaves just like [`Self::try_from_bytes`] but for [`String`]
+    /// as the respective source type.
+    ///
+    /// # Errors
+    ///
+    /// - [`sysexits::ExitCode::DataErr`]
+    fn try_from_string(&mut self, string: &str) -> Result<()>;
 
-  /// Convert this buffer into a [`Vec<u8>`].
-  ///
-  /// Some writing processes require the data to write as a sequence of UTF-8
-  /// encoded characters.  If required, this method will take care about the
-  /// provision of the data in the required type.
-  ///
-  /// # Errors
-  ///
-  /// - [`sysexits::ExitCode::DataErr`]
-  fn try_into_bytes(&self) -> Result<Vec<u8>>;
+    /// Convert this buffer into a [`Vec<u8>`].
+    ///
+    /// Some writing processes require the data to write as a sequence of UTF-8
+    /// encoded characters.  If required, this method will take care about the
+    /// provision of the data in the required type.
+    ///
+    /// # Errors
+    ///
+    /// - [`sysexits::ExitCode::DataErr`]
+    fn try_into_bytes(&self) -> Result<Vec<u8>>;
 
-  /// Convert this buffer into a [`String`].
-  ///
-  /// This method behaves just like [`Self::try_into_bytes`] but for [`String`]
-  /// as the respective target type.
-  ///
-  /// # Errors
-  ///
-  /// - [`sysexits::ExitCode::DataErr`]
-  fn try_into_string(&self) -> Result<String>;
+    /// Convert this buffer into a [`String`].
+    ///
+    /// This method behaves just like [`Self::try_into_bytes`] but for [`String`]
+    /// as the respective target type.
+    ///
+    /// # Errors
+    ///
+    /// - [`sysexits::ExitCode::DataErr`]
+    fn try_into_string(&self) -> Result<String>;
 }
 
 impl Buffer for String {
-  fn try_from_bytes(&mut self, bytes: &[u8]) -> Result<()> {
-    Self::from_utf8(bytes.to_vec()).map_or(Err(ExitCode::DataErr), |string| {
-      *self = string;
-      Ok(())
-    })
-  }
+    fn try_from_bytes(&mut self, bytes: &[u8]) -> Result<()> {
+        Self::from_utf8(bytes.to_vec()).map_or(
+            Err(ExitCode::DataErr),
+            |string| {
+                *self = string;
+                Ok(())
+            },
+        )
+    }
 
-  fn try_from_string(&mut self, string: &str) -> Result<()> {
-    *self = string.to_string();
-    Ok(())
-  }
+    fn try_from_string(&mut self, string: &str) -> Result<()> {
+        *self = string.to_string();
+        Ok(())
+    }
 
-  fn try_into_bytes(&self) -> Result<Vec<u8>> {
-    Ok(self.as_bytes().to_vec())
-  }
+    fn try_into_bytes(&self) -> Result<Vec<u8>> {
+        Ok(self.as_bytes().to_vec())
+    }
 
-  fn try_into_string(&self) -> Result<String> {
-    Ok(self.clone())
-  }
+    fn try_into_string(&self) -> Result<String> {
+        Ok(self.clone())
+    }
 }
 
 impl Buffer for Vec<u8> {
-  fn try_from_bytes(&mut self, bytes: &[u8]) -> Result<()> {
-    *self = bytes.to_vec();
-    Ok(())
-  }
+    fn try_from_bytes(&mut self, bytes: &[u8]) -> Result<()> {
+        *self = bytes.to_vec();
+        Ok(())
+    }
 
-  fn try_from_string(&mut self, string: &str) -> Result<()> {
-    *self = string.as_bytes().to_vec();
-    Ok(())
-  }
+    fn try_from_string(&mut self, string: &str) -> Result<()> {
+        *self = string.as_bytes().to_vec();
+        Ok(())
+    }
 
-  fn try_into_bytes(&self) -> Result<Vec<u8>> {
-    Ok(self.clone())
-  }
+    fn try_into_bytes(&self) -> Result<Vec<u8>> {
+        Ok(self.clone())
+    }
 
-  fn try_into_string(&self) -> Result<String> {
-    String::from_utf8(self.clone()).map_or(Err(ExitCode::DataErr), Ok)
-  }
+    fn try_into_string(&self) -> Result<String> {
+        String::from_utf8(self.clone()).map_or(Err(ExitCode::DataErr), Ok)
+    }
 }
 
 /******************************************************************************/
