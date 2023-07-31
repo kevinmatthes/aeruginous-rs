decd936869b7cb0a59892270706f3e63f815ddc5
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-07-31 19:19:59 +0200

    Update convert_buffer.rs

--- src/traits/convert_buffer.rs
+++ src/traits/convert_buffer.rs
@@ -21,94 +21,94 @@ use sysexits::{ExitCode, Result};
 
 /// A buffer that can be converted from and into another buffer type.
 pub trait ConvertBuffer<T> {
-  /// Fill this instance with the data of the given buffer, if possible.
-  ///
-  /// # Examples
-  ///
-  /// ```rust
-  /// use aeruginous::ConvertBuffer;
-  ///
-  /// let buffer = "buffer".to_string();
-  /// let mut string = "string".to_string();
-  ///
-  /// assert_eq!(string, "string".to_string());
-  /// assert_eq!(string.convert_from(buffer), Ok(()));
-  /// assert_eq!(string, "buffer".to_string());
-  /// ```
-  ///
-  /// # Errors
-  ///
-  /// - [`sysexits::ExitCode::DataErr`]
-  fn convert_from(&mut self, buffer: T) -> Result<()>;
+    /// Fill this instance with the data of the given buffer, if possible.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use aeruginous::ConvertBuffer;
+    ///
+    /// let buffer = "buffer".to_string();
+    /// let mut string = "string".to_string();
+    ///
+    /// assert_eq!(string, "string".to_string());
+    /// assert_eq!(string.convert_from(buffer), Ok(()));
+    /// assert_eq!(string, "buffer".to_string());
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// - [`sysexits::ExitCode::DataErr`]
+    fn convert_from(&mut self, buffer: T) -> Result<()>;
 
-  /// Convert this buffer into another buffer type for a moment, if possible.
-  ///
-  /// This method will attempt to convert this instance into the requested type.
-  /// In case of success, the resulting data will be returned.  This instance
-  /// will remain unchanged in either case.
-  ///
-  /// # Examples
-  ///
-  /// ```rust
-  /// use aeruginous::ConvertBuffer;
-  ///
-  /// let string = "string".to_string();
-  ///
-  /// assert_eq!(string, "string".to_string());
-  /// assert_eq!(string.convert_into(), Ok(b"string".to_vec()));
-  /// assert_eq!(string, "string".to_string());
-  /// ```
-  ///
-  /// # Errors
-  ///
-  /// - [`sysexits::ExitCode::DataErr`]
-  fn convert_into(&self) -> Result<T>;
+    /// Convert this buffer into another buffer type for a moment, if possible.
+    ///
+    /// This method will attempt to convert this instance into the requested type.
+    /// In case of success, the resulting data will be returned.  This instance
+    /// will remain unchanged in either case.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use aeruginous::ConvertBuffer;
+    ///
+    /// let string = "string".to_string();
+    ///
+    /// assert_eq!(string, "string".to_string());
+    /// assert_eq!(string.convert_into(), Ok(b"string".to_vec()));
+    /// assert_eq!(string, "string".to_string());
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// - [`sysexits::ExitCode::DataErr`]
+    fn convert_into(&self) -> Result<T>;
 }
 
 impl ConvertBuffer<Self> for String {
-  fn convert_from(&mut self, buffer: Self) -> Result<()> {
-    *self = buffer;
-    Ok(())
-  }
+    fn convert_from(&mut self, buffer: Self) -> Result<()> {
+        *self = buffer;
+        Ok(())
+    }
 
-  fn convert_into(&self) -> Result<Self> {
-    Ok(self.clone())
-  }
+    fn convert_into(&self) -> Result<Self> {
+        Ok(self.clone())
+    }
 }
 
 impl ConvertBuffer<Vec<u8>> for String {
-  fn convert_from(&mut self, buffer: Vec<u8>) -> Result<()> {
-    Self::from_utf8(buffer).map_or(Err(ExitCode::DataErr), |s| {
-      *self = s;
-      Ok(())
-    })
-  }
+    fn convert_from(&mut self, buffer: Vec<u8>) -> Result<()> {
+        Self::from_utf8(buffer).map_or(Err(ExitCode::DataErr), |s| {
+            *self = s;
+            Ok(())
+        })
+    }
 
-  fn convert_into(&self) -> Result<Vec<u8>> {
-    Ok(self.as_bytes().to_vec())
-  }
+    fn convert_into(&self) -> Result<Vec<u8>> {
+        Ok(self.as_bytes().to_vec())
+    }
 }
 
 impl ConvertBuffer<Self> for Vec<u8> {
-  fn convert_from(&mut self, buffer: Self) -> Result<()> {
-    *self = buffer;
-    Ok(())
-  }
+    fn convert_from(&mut self, buffer: Self) -> Result<()> {
+        *self = buffer;
+        Ok(())
+    }
 
-  fn convert_into(&self) -> Result<Self> {
-    Ok(self.clone())
-  }
+    fn convert_into(&self) -> Result<Self> {
+        Ok(self.clone())
+    }
 }
 
 impl ConvertBuffer<String> for Vec<u8> {
-  fn convert_from(&mut self, buffer: String) -> Result<()> {
-    *self = buffer.as_bytes().to_vec();
-    Ok(())
-  }
+    fn convert_from(&mut self, buffer: String) -> Result<()> {
+        *self = buffer.as_bytes().to_vec();
+        Ok(())
+    }
 
-  fn convert_into(&self) -> Result<String> {
-    String::from_utf8(self.clone()).map_or(Err(ExitCode::DataErr), Ok)
-  }
+    fn convert_into(&self) -> Result<String> {
+        String::from_utf8(self.clone()).map_or(Err(ExitCode::DataErr), Ok)
+    }
 }
 
 /******************************************************************************/
