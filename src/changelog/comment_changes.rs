42435a308a61663600770de3d1b5d8b89152893f
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2024-01-18 22:05:25 +0100

    Deprecated ::= comment-changes:  option ``-S``

--- src/changelog/comment_changes.rs
+++ src/changelog/comment_changes.rs
@@ -17,6 +17,7 @@
 |                                                                              |
 \******************************************************************************/
 
+
 use crate::{FragmentExportFormat, PatternWriter, ToMd, ToRon, ToRst, ToXml};
 use git2::{Oid, Repository};
 use sysexits::{ExitCode, Result};
